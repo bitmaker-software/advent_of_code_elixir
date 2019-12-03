5ad68079b6e62046691d229fee6c5d60ffbaf6a5
Ricardo Fernandes <ricardo.fernandes@bitmaker-software.com>
2019-12-03 12:22:07 +0000

    Solving part #2 of day #1!

--- lib/2019/one.ex
+++ lib/2019/one.ex
@@ -1,7 +1,11 @@
 defmodule AdventOfCode.Y2019.One do
   alias AdventOfCode.Tools
   
-  def compute(mass) when mass <= 0, do: 0
+  #
+  # Part #1
+  #
+
+  def compute(mass) when mass < 6, do: 0
   def compute(mass) do
     floor(mass / 3) - 2
   end
@@ -14,6 +18,31 @@ defmodule AdventOfCode.Y2019.One do
   def run() do
     Tools.read_int_lines_from_console()
     |> compute_all()
+    |> IO.inspect(label: "Result")
+  end
+
+  #
+  # Part #2
+  #
+
+  def compute_fuel_4_fuel(fuel_mass, total) when fuel_mass < 6, do: total
+  def compute_fuel_4_fuel(fuel_mass, total) do
+    compute_2(fuel_mass, total)
+  end
+
+  def compute_2(mass, total \\ 0) do
+    fuel_mass = compute(mass)
+    fuel_mass + compute_fuel_4_fuel(fuel_mass, total)
+  end
+
+  def compute_all_2(masses) do
+    masses
+    |> Enum.reduce(0, fn(x, acc) -> compute_2(x) + acc end)
+  end
+
+  def run_2() do
+    Tools.read_int_lines_from_console()
+    |> compute_all_2()
     |> IO.inspect(label: "Result")
   end
 end
--- test/2019/one_test.exs
+++ test/2019/one_test.exs
@@ -3,6 +3,8 @@ defmodule AdventOfCode.Y2019.OneTest do
   alias AdventOfCode.Y2019.One
   doctest AdventOfCode.Y2019.One
 
+  # Edge cases
+
   test "check negative mass" do
     assert One.compute(-1) == 0
   end
@@ -10,6 +12,16 @@ defmodule AdventOfCode.Y2019.OneTest do
   test "check zero mass" do
     assert One.compute(0) == 0
   end
+
+  test "check 8" do
+    assert One.compute(8) == 0
+  end
+
+  test "check 9" do
+    assert One.compute(9) == 1
+  end
+
+  # Samples
 
   test "check 12" do
     assert One.compute(12) == 2
@@ -28,6 +40,28 @@ defmodule AdventOfCode.Y2019.OneTest do
   end
 
   test "check sum" do
-    assert One.compute_all([-1, 0, 12, 14, 1969, 100756]) == 34241
+    assert One.compute_all([-1, 0, 8, 12, 14, 1969, 100756]) == 34241
+  end
+
+  # Part #2
+
+  test "check #2 negative" do
+    assert One.compute_2(-1) == 0
+  end
+
+  test "check #2 14" do
+    assert One.compute_2(14) == 2
+  end
+
+  test "check #2 1969" do
+    assert One.compute_2(1969) == 966
+  end
+
+  test "check #2 100756" do
+    assert One.compute_2(100756) == 50346
+  end
+
+  test "check #2 sum" do
+    assert One.compute_all_2([-1, 8, 14, 1969, 100756]) == 51314
   end
 end
